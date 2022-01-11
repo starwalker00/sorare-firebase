843ffea2ace61611e5b325778c5e4061b59ef78f
starwalker00 <aaronwalkerup@gmail.com>
2022-01-11 19:13:01 +0100

    cleanup

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-Calls the GraphQl Sorare API.
+Calls the GraphQL Sorare API.
 Send notification to android flutter app via Firebase.
 
 ## Configuration
@@ -10,10 +10,19 @@ Send notification to android flutter app via Firebase.
 Git Clone.
 Run npm install.
 Import serviceAccountKey.json from Firebase web console project.
+Fill androidToken.js with FCM tokens.
 Configure loop delays in index.js.
 Run npm start.
 
 ## Useful links :
 
+### Sorare API
+
+- https://github.com/sorare/api
+- https://api.sorare.com/graphql/playground
+
+### Firebase
+
 - https://firebase.google.com/docs/functions/get-started
 - https://firebase.google.com/docs/cloud-messaging/send-message#send-messages-to-multiple-devices
+
--- src/firebaseService.js
+++ src/firebaseService.js
@@ -1,14 +1,11 @@
 var admin = require("firebase-admin");
 var serviceAccount = require("../config/serviceAccountKey.json");
 
+var androidToken = require("../config/androidToken.js");
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
-
-const registrationTokens = [
-    'enQ3XUOORB2-YfUbKqUAVN:APA91bELJ_7xZE3ourPYOeGaspuicqlhwHzVXPmhSlIPwNJh-a6z5uAWShXqsabnYh6WSPVNeYA4lVr2rLqTyRx0v6q6lj1k_hLUhEQ0CkewYxpnIn3JRCaBYs1RTVtSoGv5aXKyvJxu',
-    'enQ3XUOORB2-YfUbKqUAVN:APA91bELJ_7xZE3ourPYOeGaspuicqlhwHzVXPmhSlIPwNJh-a6z5uAWShXqsabnYh6WSPVNeYA4lVr2rLqTyRx0v6q6lj1k_hLUhEQ0CkewYxpnIn3JRCaBYs1RTVtSoGv5aXKyvJxu',
-];
 
 // let message = {
 //     notification: {
@@ -22,7 +19,7 @@ let options = {
 };
 
 function sendToDevice(message) {
-    admin.messaging().sendToDevice(registrationTokens, message, options)
+    admin.messaging().sendToDevice(androidToken.registrationTokens, message, options)
         .then((response) => {
             console.log(response.successCount + ' messages were sent successfully');
         })
